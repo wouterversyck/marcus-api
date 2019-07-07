ec071ddb968fbf050b80f57f7c625442273cf8a4
WouterVersyck <wouter.versyck@gmail.com>
2019-07-07 11:51:49 +0200

    Initial commit

---
+++ src/main/java/be/wouterversyck/shoppinglistapi/ShoppingListApiApplication.java
@@ -0,0 +1,13 @@
+package be.wouterversyck.shoppinglistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingListApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingListApiApplication.class, args);
+    }
+
+}
