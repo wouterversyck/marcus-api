d0c0140d878500ee17be3a660d65c6dda5f5835a
WouterVersyck <wouter.versyck@gmail.com>
2019-08-11 17:44:57 +0200

    Added shopping list funcionality

---
+++ src/main/java/be/wouterversyck/shoppinglistapi/shoppinglist/controllers/ShoppingListController.java
@@ -0,0 +1,33 @@
+package be.wouterversyck.shoppinglistapi.shoppinglist.controllers;
+
+import be.wouterversyck.shoppinglistapi.shoppinglist.models.ShoppingList;
+import be.wouterversyck.shoppinglistapi.shoppinglist.services.ShoppingListService;
+import be.wouterversyck.shoppinglistapi.users.models.User;
+import be.wouterversyck.shoppinglistapi.users.services.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/shoppinglist")
+public class ShoppingListController {
+    private ShoppingListService shoppingListService;
+    private UserService userService;
+
+    public ShoppingListController(ShoppingListService shoppingListService, UserService userService) {
+        this.shoppingListService = shoppingListService;
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<ShoppingList> getShoppingList(HttpServletRequest request) {
+        Principal principal = request.getUserPrincipal();
+        User user = userService.getUserByUsername(principal.getName());
+
+        return shoppingListService.getShoppingListsForUser(user);
+    }
+}
---
+++ src/main/java/be/wouterversyck/shoppinglistapi/shoppinglist/daos/ShoppingListDao.java
@@ -0,0 +1,12 @@
+package be.wouterversyck.shoppinglistapi.shoppinglist.daos;
+
+import be.wouterversyck.shoppinglistapi.shoppinglist.models.ShoppingList;
+import be.wouterversyck.shoppinglistapi.users.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ShoppingListDao extends JpaRepository<ShoppingList, Long> {
+
+    List<ShoppingList> findAllByOwner(User user);
+}
