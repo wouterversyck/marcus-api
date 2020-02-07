20f333d31e44363da992039f52682355301b906b
wouter Versyck <wouter.versyck@designisdead.com>
2020-02-07 19:21:12 +0100

    Fix caching and simplify integration test

---
+++ src/main/java/be/wouterversyck/shoppinglistapi/ApplicationConfiguration.java
@@ -0,0 +1,9 @@
+package be.wouterversyck.shoppinglistapi;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class ApplicationConfiguration {
+}
