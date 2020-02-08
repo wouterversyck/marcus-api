1963f3ae76f24c0b74bf7c38cbc8fec135afe657
Wouter Versyck <wouter.versyck@gmail.com>
2020-02-08 14:27:10 +0100

    Log cache miss

--- src/main/java/be/wouterversyck/shoppinglistapi/shoppinglist/services/ShoppingListService.java
+++ src/main/java/be/wouterversyck/shoppinglistapi/shoppinglist/services/ShoppingListService.java
@@ -4,11 +4,13 @@ import be.wouterversyck.shoppinglistapi.shoppinglist.ShoppingListNotFoundExcepti
 import be.wouterversyck.shoppinglistapi.shoppinglist.daos.ShoppingListDao;
 import be.wouterversyck.shoppinglistapi.shoppinglist.models.ShoppingListDto;
 import be.wouterversyck.shoppinglistapi.users.models.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class ShoppingListService {
 
@@ -20,11 +22,13 @@ public class ShoppingListService {
 
     @Cacheable("be.wouterversyck.shoppinglistapi.shoppinglist.findforuser")
     public List<ShoppingListDto> getShoppingListsForUser(final User user) {
+        log.info("Fetching shopping lists form db (cache miss)");
         return shoppingListDao.findAllByOwner(user);
     }
 
     @Cacheable(value = "be.wouterversyck.shoppinglistapi.shoppinglist.findbyid", key = "#id")
     public ShoppingListDto getShoppingListById(final long id, final User user) throws ShoppingListNotFoundException {
+        log.info("Fetching shopping lists form db (cache miss)");
         return shoppingListDao.findByIdAndOwner(id, user).orElseThrow(
                 () -> new ShoppingListNotFoundException(id)
         );
