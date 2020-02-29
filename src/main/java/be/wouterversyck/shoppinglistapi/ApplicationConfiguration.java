53b73e0475b8ec6a493329cb779edc7032cab33f
Wouter Versyck <wouter.versyck@gmail.com>
2020-02-29 15:35:41 +0100

    Revert "Added sentry"
    This reverts commit aa09f1568fbaf6ef2b186c91783567cdc9927aef.

--- src/main/java/be/wouterversyck/shoppinglistapi/ApplicationConfiguration.java
+++ src/main/java/be/wouterversyck/shoppinglistapi/ApplicationConfiguration.java
@@ -1,28 +1,9 @@
 package be.wouterversyck.shoppinglistapi;
 
-import io.sentry.Sentry;
-import io.sentry.spring.SentryExceptionResolver;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.web.servlet.HandlerExceptionResolver;
 
 @Configuration
 @EnableCaching
 public class ApplicationConfiguration {
-
-    private final String sentryDsn;
-
-    public ApplicationConfiguration(@Value("${sentry.dsn:''}") final String sentryDsn) {
-        this.sentryDsn = sentryDsn;
-    }
-
-    @Bean
-    @Profile("prd")
-    public HandlerExceptionResolver sentryExceptionResolver() {
-        Sentry.init(sentryDsn);
-        return new SentryExceptionResolver();
-    }
 }
