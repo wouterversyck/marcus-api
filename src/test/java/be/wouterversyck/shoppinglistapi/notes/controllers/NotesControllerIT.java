64b0cb19055990950f0830ddc88c4a84439e4316
Wouter Versyck <wouter.versyck@gmail.com>
2020-07-11 14:22:32 +0200

    Fix integration test

--- src/test/java/be/wouterversyck/shoppinglistapi/notes/controllers/NotesControllerIT.java
+++ src/test/java/be/wouterversyck/shoppinglistapi/notes/controllers/NotesControllerIT.java
@@ -1,9 +1,11 @@
 package be.wouterversyck.shoppinglistapi.notes.controllers;
 
 import be.wouterversyck.shoppinglistapi.AbstractIT;
+import be.wouterversyck.shoppinglistapi.notes.models.ShoppingList;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
-import static java.lang.String.format;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -11,14 +13,21 @@ class NotesControllerIT extends AbstractIT {
 
     private static final String SHOPPING_LIST_URL = "/shoppinglist/";
 
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
     @Test
     void shouldSee404_WhenITryToSeeAnotherUsersNotes() throws Exception {
         String token = login("user", "password");
+        var list = ShoppingList.builder()
+                .id("3")
+                .owner(2L)
+                .name("NOTE_NAME").build();
+        mongoTemplate.save(list);
 
-        // 3 is owned by admin, see liquibase seed data
         getMvc()
                 .perform(
-                        getWithToken(format("%s%s", SHOPPING_LIST_URL, 3), token))
+                        getWithToken(SHOPPING_LIST_URL + 3, token))
                 .andExpect(status().isNotFound());
     }
 
@@ -26,11 +35,16 @@ class NotesControllerIT extends AbstractIT {
     void shouldSeeMyList_WhenImLoggedIn() throws Exception {
         String token = login("admin", "secure");
 
-        // 3 is owned by admin, see liquibase seed data
+        var list = ShoppingList.builder()
+                .id("3")
+                .owner(2L)
+                .name("NOTE_NAME").build();
+        mongoTemplate.save(list);
+
         getMvc()
                 .perform(
-                        getWithToken(format("%s%s", SHOPPING_LIST_URL, 3), token))
+                        getWithToken(SHOPPING_LIST_URL + 3, token))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name", is("List of other user")));
+                .andExpect(jsonPath("$.name", is("NOTE_NAME")));
     }
 }
